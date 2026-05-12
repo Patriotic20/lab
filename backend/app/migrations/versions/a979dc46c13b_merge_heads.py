65423d928547afacaba1b05becc42969b29f4876
Patriotic20 <pterest160@gmail.com>
2026-05-12 10:50:27 +0500

    fix: correct formatting of revision identifiers in migration script

--- backend/app/migrations/versions/a979dc46c13b_merge_heads.py
+++ backend/app/migrations/versions/a979dc46c13b_merge_heads.py
@@ -5,11 +5,12 @@ Revises: b5f3a1b6c8d2, c1f7b9a2d4e8
 Create Date: 2026-05-12 10:33:54.971650
 
 """
+
 from typing import Sequence, Union
 
 # revision identifiers, used by Alembic.
-revision: str = 'a979dc46c13b'
-down_revision: Union[str, Sequence[str], None] = ('b5f3a1b6c8d2', 'c1f7b9a2d4e8')
+revision: str = "a979dc46c13b"
+down_revision: Union[str, Sequence[str], None] = ("b5f3a1b6c8d2", "c1f7b9a2d4e8")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
